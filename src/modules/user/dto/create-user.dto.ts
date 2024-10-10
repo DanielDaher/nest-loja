0dd3d16ad953c13412246d39583e4d227bb40c8f
Daniel <danielfdoficial@gmail.com>
2024-10-10 17:32:55 -0300

    feat: installing and using pipe VALIDATORS

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
@@ -8,23 +15,30 @@ export class CreateUserDto {
   role!: string;
 
   @ApiProperty({ example: 'John Doe' })
+  @IsString({ message: '$property deve ser string!' }) // personalizando a mensagem de erro e acessando a prorpriedade
   name!: string;
 
   @ApiProperty({ example: 'client@getnada.com' })
+  @IsEmail(undefined, { message: '$property informado é inválido' })
   email!: string;
 
   @ApiProperty({ example: '123456789' })
+  @MinLength(4)
   password!: string;
 
   @ApiProperty({ example: '12345678910' })
+  @MinLength(11)
   cpf!: string;
 
   @ApiProperty({ example: '(31) 99999-9999' })
+  @MinLength(10)
+  @MaxLength(25)
   phone!: string;
 
   @ApiProperty({ example: 'https://example.com/profile.jpg' })
   imageUrl!: string;
 
   @ApiProperty({ example: 'ACTIVE' })
+  @IsNotEmpty({ message: 'o campo $property precisa estar preenchido' })
   status!: string;
 }
