c05abcdb77474dd9d4baeee9e67fbdf7abb91a70
Daniel <danielfdoficial@gmail.com>
2024-10-11 17:11:10 -0300

    feat: create and use prisma service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
