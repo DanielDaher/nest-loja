d8a3bb1bc828a18877a58c616431e5aa208cd119
Daniel <danielfdoficial@gmail.com>
2024-12-16 12:02:28 -0300

    build: fix user module provider dependency

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserDAO } from './user.dao';
+import { PasswordService } from 'src/shared/services/password/password.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserDAO],
+  providers: [UserService, UserDAO, PasswordService],
 })
 export class UserModule {}
